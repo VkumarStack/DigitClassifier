fcbaf110d771205044e7d36406103d1cd7de899e
VkumarStack <vksogi@gmail.com>
2023-05-15 09:48:47 -0700

    Update model - 97.23 accuracy

--- Digit_Classifier.py
+++ Digit_Classifier.py
@@ -8,18 +8,20 @@ from PIL import Image, ImageOps
 # This is your neural network class, so it must extend nn.Module
 # For your final submission you will be submitting this cell as its own file
 # REMEMBER TO REPLACE X IN THE CLASS NAME WITH YOUR TEAM NUMBER
+# This is your neural network class, so it must extend nn.Module
+# For your final submission you will be submitting this cell as its own file
+# REMEMBER TO REPLACE X IN THE CLASS NAME WITH YOUR TEAM NUMBER
 class Digit_Classifiertbd(nn.Module):
   def __init__(self):
     # Handle some under-the-hood PyTorch stuff
     super().__init__()
     # Now put your layers below in addition to any other member variables you need
-    self.conv1 = nn.Conv2d(1, 6, 5)
-    self.pool = nn.MaxPool2d(2, 2)
-    self.conv2 = nn.Conv2d(6, 24, 5)
-    self.conv3 = nn.Conv2d(16, 30, 5)
-    self.flat = nn.Flatten()
-    self.layer1 = nn.Linear(24 * 4 * 4, 100)
     self.relu = nn.ReLU()
+    self.pool = nn.MaxPool2d(2, 2)
+    self.flat = nn.Flatten()
+    self.conv1 = nn.Conv2d(1, 6, 5)
+    self.conv2 = nn.Conv2d(6, 16, 5)
+    self.layer1 = nn.Linear(16 * 4 * 4, 100)
     self.layer2 = nn.Linear(100, 80)
     self.layer3 = nn.Linear(80, 10)
 
@@ -51,12 +53,3 @@ class Model:
 
     def predict(self, img):
        return torch.argmax(self.model(torch.reshape(self.convert_tensor(img), (1, 1, 28, 28))))
-
-# img = Image.open("./test.png")
-# img = ImageOps.invert(img)
-# convert_tensor = torchvision.transforms.ToTensor()
-# img = convert_tensor(img)
-# img = torch.reshape(img, (1, 28, 28))
-
-# plt.imshow(img.numpy().squeeze(), cmap="gray_r")
-# plt.show()
